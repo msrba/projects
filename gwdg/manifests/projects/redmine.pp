7ec27ac70ac4056b787e2cc1df2d0db027808980
Maik Srba <maik.srba@gwdg.de>
2015-02-26 16:36:00 +0100

    add first composite layer

--- gwdg/manifests/projects/redmine.pp
+++ gwdg/manifests/projects/redmine.pp
@@ -4,4 +4,8 @@ class gwdg::projects::redmine(
   
   include gwdg::projects::base
 
+  class { 'apache': }
+  class { 'apache::mod::passenger': }
+  class { '::mysql::server': }
+  class { 'redmine': }
 }
